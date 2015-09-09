ba066fdc75c728975055b4c613f341f1e6ccd2fa
josdem <joseluis.delacruz@gmail.com>
2015-09-09 14:48:37 -0500

    #1 Modificando el RequestMapping del controller

--- src/main/groovy/com/tim/modulusuno/sepomex/controller/SepomexController.groovy
+++ src/main/groovy/com/tim/modulusuno/sepomex/controller/SepomexController.groovy
@@ -2,12 +2,14 @@ package com.tim.modulusuno.sepomex
 
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
 
 @RestController
+@RequestMapping("/sepomex")
 class SepomexController {
 
-  @RequestMapping("/")
-  String index() {
+  @RequestMapping(value="/show", method=RequestMethod.GET)
+  String show() {
     "Greetings from Spring Boot!"
   }
 

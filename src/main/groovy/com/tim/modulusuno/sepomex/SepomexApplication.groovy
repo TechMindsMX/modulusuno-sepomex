421ec6e84ef94d105ff3f5f14b665ff7b7c3cbcd
josdem <joseluis.delacruz@gmail.com>
2015-11-19 15:23:32 -0600

    Agregando compatibilidad con war usando el SpringBootServletInitializer

---
+++ src/main/groovy/com/tim/modulusuno/sepomex/SepomexApplication.groovy
@@ -0,0 +1,14 @@
+package com.tim.modulusuno.sepomex
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.web.SpringBootServletInitializer
+
+@SpringBootApplication
+class SepomexApplication extends SpringBootServletInitializer {
+
+  static void main(String[] args) {
+    SpringApplication.run SepomexApplication.class, args
+  }
+
+}
